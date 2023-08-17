23233faf01bf052dbd14efa708818575af3cfd2c
Donato Zollo <davinazarra@gmail.com>
2023-08-17 19:53:51 -0300

    contact ready

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Link as Anchor } from 'react-router-dom'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='min-h-[83.9vh]'>
+      <div className='flex flex-col justify-center items-center my-2'>
+        <p className='font-bold text-2xl text-slate-800'>Contact</p>
+        <p className='font-semibold text-xl text-slate-800'>You can ask us what you need</p>
+      </div>
+      <div className='flex flex-col items-center'>
+        <img src="/playing.jpg" alt="Playing" className='w-[40%] my-2' />
+        <p className='font-semibold text-slate-800'>You can ask us what day from 2:00 pm until 7:00 pm.</p>
+        <div className='flex justify-between w-[40%] my-4'>
+          <Anchor className='font-semibold text-blue-700'>E-Mail: mindygames@mh.com</Anchor>
+          <Anchor className='font-semibold text-blue-700'>Phone: 001112711</Anchor>
+        </div>
+      </div>  
+    </div>
   )
 }
