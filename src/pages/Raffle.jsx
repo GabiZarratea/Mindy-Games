83dc33d36ff555f84f51e6c2c49ef0153a7dab3d
Donato Zollo <davinazarra@gmail.com>
2023-09-15 19:51:33 -0300

    mercadopago

--- src/pages/Raffle.jsx
+++ src/pages/Raffle.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Swal from 'sweetalert2';
 import { api, apiUrl, endpoints } from '../utils/api.js'
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react'
+import axios from 'axios';
 
 export default function Raffle() {
     
@@ -8,6 +10,26 @@ export default function Raffle() {
   const [tournaments, setTournaments] = useState([]);
   const [availableGames, setAvailableGames] = useState([]);
   const [availableDates, setAvailableDates] = useState([]);
+  const [preferenceId, setPreferenceId] = useState(null)
+
+  initMercadoPago('TEST-99eaf0b7-ed7a-46f3-bd86-4dfbb73509d2');
+
+  const createPreference = async () => {
+    try {
+      const response = await axios.post("http://localhost:8080/api/mercadopago/create_preference", {
+        description: "Producto X",
+        price: 1,
+        quantity: 1,
+      })
+
+      const { id } = response.data;
+      console.log(id)
+      return id;
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
 
   const fetchTournaments = async () => {
     try {
@@ -25,21 +47,26 @@ export default function Raffle() {
     fetchTournaments()
   }, []);
 
-  const handleClick = (event) => {
+  const handleClick = async () => {
     event.preventDefault()
 
-    console.log(('Form data submitted:', formData))
-    Swal.fire({
-      title: 'The data was sent correctly, we will contact you!.',
-      width: 600,
-      icon: 'success',
-      iconColor: '#FF76E6',
-      padding: '3em',
-      color: '#4C4C4C',
-      confirmButtonColor: '#FF76E6',
-      background: '#F1C6FB',
-      backdrop: `rgba(0,0,123,0.4) url(/cats.gif) left top no-repeat`,
-    })
+    const id = await createPreference()
+    if (id) {
+      setPreferenceId(id)
+    }
+
+    // console.log(('Form data submitted:', formData))
+    // Swal.fire({
+    //   title: 'The data was sent correctly, we will contact you!.',
+    //   width: 600,
+    //   icon: 'success',
+    //   iconColor: '#FF76E6',
+    //   padding: '3em',
+    //   color: '#4C4C4C',
+    //   confirmButtonColor: '#FF76E6',
+    //   background: '#F1C6FB',
+    //   backdrop: `rgba(0,0,123,0.4) url(/cats.gif) left top no-repeat`,
+    // })
   }
 
   const handleChange = (event) => {
@@ -62,6 +89,8 @@ export default function Raffle() {
     }
   };
 
+  
+
   return (
     <div className='min-h-[84.9vh] flex flex-col'>
       <div className='flex flex-col justify-center items-center my-2'>
@@ -70,8 +99,9 @@ export default function Raffle() {
       </div>
       <div className='flex flex-row justify-around'>
         <div className='flex flex-col'>
-          <p className='text-md font-semibold text-slate-700 text-center mb-2'>Our location where the tournaments are held is at</p>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d450940.6550535176!2d-82.7839533166323!3d27.994695904415046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c2b782b3b9d1e1%3A0xa75f1389af96b463!2sTampa%2C%20Florida%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sar!4v1684418824838!5m2!1ses-419!2sar" className='w-[500px] h-[500px]' allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            <div id="wallet_container" onClick={handleClick}>
+              <Wallet initialization={{ preferenceId, redirectMode: 'modal' }} />
+            </div>
         </div>
         <div>
           <form>
@@ -134,11 +164,10 @@ export default function Raffle() {
                 <input type="checkbox" value={formData.acceptTerms} name='acceptTerms' onChange={handleChange}/>
                 <label htmlFor="accept" className='text-sm ms-2 font-semibold text-slate-600'>I accept terms and conditions...</label>
               </div>
-              <button className='rounded-md bg- bg-fuchsia-200 p-2 px-6 my-4' type='submit' onClick={handleClick}> Submit </button>
             </div>
           </form>
         </div>
-      </div>  
+      </div>
     </div>
   )
 }
